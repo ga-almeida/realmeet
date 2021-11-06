b7fa201e102d140e776ce9115418b31fc6e80e2f
Gabriel Almeida <gabrielsantos.almeida@hotmail.com>
2021-11-06 15:30:11 -0300

    create RoomMapper with the API MapStruct

--- service/src/main/java/br/com/sw2you/realmeet/RoomController.java
+++ service/src/main/java/br/com/sw2you/realmeet/RoomController.java
@@ -4,6 +4,7 @@ import static java.util.concurrent.CompletableFuture.supplyAsync;
 
 import br.com.sw2you.realmeet.api.facade.RoomsApi;
 import br.com.sw2you.realmeet.api.model.RoomDTO;
+import br.com.sw2you.realmeet.mapper.RoomMapper;
 import br.com.sw2you.realmeet.service.RoomService;
 import java.util.concurrent.CompletableFuture;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +15,7 @@ public class RoomController implements RoomsApi {
 
     private final RoomService roomService;
 
-    public RoomController(RoomService roomService) {
+    public RoomController(RoomService roomService, RoomMapper roomMapper) {
         this.roomService = roomService;
     }
 
--- service/src/main/java/br/com/sw2you/realmeet/service/RoomService.java
+++ service/src/main/java/br/com/sw2you/realmeet/service/RoomService.java
@@ -6,21 +6,24 @@ import br.com.sw2you.realmeet.api.model.RoomDTO;
 import br.com.sw2you.realmeet.domain.entity.Room;
 import br.com.sw2you.realmeet.domain.repository.RoomRepository;
 import br.com.sw2you.realmeet.exception.RoomNotFoundException;
+import br.com.sw2you.realmeet.mapper.RoomMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RoomService {
 
     private RoomRepository roomRepository;
+    private final RoomMapper roomMapper;
 
-    public RoomService(RoomRepository roomRepository) {
+    public RoomService(RoomRepository roomRepository, RoomMapper roomMapper) {
         this.roomRepository = roomRepository;
+        this.roomMapper = roomMapper;
     }
 
     public RoomDTO find(Long id) {
         requireNonNull(id);
         Room room = roomRepository.findById(id).orElseThrow(RoomNotFoundException::new);
 
-        return new RoomDTO().id(room.getId()).name(room.getName()).seats(room.getSeats());
+        return roomMapper.fromEntityToDTO(room);
     }
 }
