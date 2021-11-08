8221835a340e55f3c7916483d4c6e713e406de09
ga-almeida <gabrielsantos.almeida@hotmail.com>
2021-11-08 11:25:52 -0300

    create struct for suport integration tests

---
+++ service/src/test/java/br/com/sw2you/realmeet/core/BaseIntegrationTest.java
@@ -0,0 +1,45 @@
+package br.com.sw2you.realmeet.core;
+
+import br.com.sw2you.realmeet.Application;
+import br.com.sw2you.realmeet.api.ApiClient;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeEach;
+
+// Delegando que a classe vai utilizar o arquivo de configuração applicaton-integration-test
+@ActiveProfiles(profiles = "integration-test")
+// Deixando o Spring gerencie essa classe
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Application.class)
+public abstract class BaseIntegrationTest {
+
+    @Autowired
+    private Flyway flyway;
+
+    // Spring injeta dentro dessa variavel a porta randomica
+    @LocalServerPort
+    private int serverPort;
+
+    @BeforeEach
+    void setup() {
+        setupFlyway();
+        setupEach();
+    }
+
+    // função onde as classes filhas vão sobrescrever
+    protected void setupEach() {}
+
+    // setando a url do servidor
+    protected void setLocalHostBasePath(ApiClient apiClient, String path) throws MalformedURLException {
+        apiClient.setBasePath(new URL("http", "localhost", serverPort, path).toString());
+    }
+
+    private void setupFlyway() {
+        flyway.clean();
+        flyway.migrate();
+    }
+}
