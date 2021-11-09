22040fe4b306bc6119735d29279c2588654f7a9f
ga-almeida <gabrielsantos.almeida@hotmail.com>
2021-11-09 11:33:49 -0300

    validate seats in the room

--- service/src/main/java/br/com/sw2you/realmeet/validator/RoomValidator.java
+++ service/src/main/java/br/com/sw2you/realmeet/validator/RoomValidator.java
@@ -1,7 +1,6 @@
 package br.com.sw2you.realmeet.validator;
 
-import static br.com.sw2you.realmeet.validator.ValidatorConstants.ROOM_NAME;
-import static br.com.sw2you.realmeet.validator.ValidatorConstants.ROOM_NAME_MAX_LENGTH;
+import static br.com.sw2you.realmeet.validator.ValidatorConstants.*;
 import static br.com.sw2you.realmeet.validator.ValidatorUtils.*;
 
 import br.com.sw2you.realmeet.api.model.CreateRoomDTO;
@@ -15,6 +14,10 @@ public class RoomValidator {
 
         validateRequired(createRoomDTO.getName(), ROOM_NAME, validationErrors);
         validateMaxLength(createRoomDTO.getName(), ROOM_NAME, ROOM_NAME_MAX_LENGTH, validationErrors);
+
+        validateRequired(createRoomDTO.getSeats(), ROOM_SEATS, validationErrors);
+        validateMinValue(createRoomDTO.getSeats(), ROOM_SEATS, ROOM_SEATS_MIN_VALUE, validationErrors);
+        validateMaxValue(createRoomDTO.getSeats(), ROOM_SEATS, ROOM_SEATS_MAX_VALUE, validationErrors);
 
         throwOnError(validationErrors);
     }
--- service/src/main/java/br/com/sw2you/realmeet/validator/ValidatorUtils.java
+++ service/src/main/java/br/com/sw2you/realmeet/validator/ValidatorUtils.java
@@ -1,7 +1,6 @@
 package br.com.sw2you.realmeet.validator;
 
-import static br.com.sw2you.realmeet.validator.ValidatorConstants.EXCEEDS_MAX_LENGTH;
-import static br.com.sw2you.realmeet.validator.ValidatorConstants.MISSING;
+import static br.com.sw2you.realmeet.validator.ValidatorConstants.*;
 import static java.util.Objects.isNull;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
@@ -39,6 +38,24 @@ public final class ValidatorUtils {
     public static boolean validateMaxLength(String field, String fieldName, int maxLength, ValidationErrors validationErrors) {
         if (!isBlank(field) && field.trim().length() > maxLength) {
             validationErrors.add(fieldName, fieldName + EXCEEDS_MAX_LENGTH);
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean validateMaxValue(Integer field, String fieldName, int maxValue, ValidationErrors validationErrors) {
+        if (!isNull(field) && field > maxValue) {
+            validationErrors.add(fieldName, fieldName + EXCEEDS_MAX_VALUE);
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean validateMinValue(Integer field, String fieldName, int minValue, ValidationErrors validationErrors) {
+        if (!isNull(field) && field < minValue) {
+            validationErrors.add(fieldName, fieldName + BELOW_MIN_VALUE);
             return false;
         }
 
