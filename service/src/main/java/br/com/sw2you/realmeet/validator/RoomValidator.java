e9b5824786869f356287de4802a97873f8aea5e6
ga-almeida <gabrielsantos.almeida@hotmail.com>
2021-11-10 22:16:10 -0300

    update room

--- service/src/main/java/br/com/sw2you/realmeet/validator/RoomValidator.java
+++ service/src/main/java/br/com/sw2you/realmeet/validator/RoomValidator.java
@@ -2,11 +2,15 @@ package br.com.sw2you.realmeet.validator;
 
 import static br.com.sw2you.realmeet.validator.ValidatorConstants.*;
 import static br.com.sw2you.realmeet.validator.ValidatorUtils.*;
+import static java.util.Objects.isNull;
 
 import br.com.sw2you.realmeet.api.model.CreateRoomDTO;
+import br.com.sw2you.realmeet.api.model.UpdateRoomDTO;
 import br.com.sw2you.realmeet.domain.repository.RoomRepository;
 import br.com.sw2you.realmeet.exception.InvalidRequestException;
+import java.util.Objects;
 import org.springframework.stereotype.Component;
+import org.apache.commons.lang3.ObjectUtils;
 
 @Component
 public class RoomValidator {
@@ -19,8 +23,17 @@ public class RoomValidator {
     public void validate(CreateRoomDTO createRoomDTO) {
         var validationErrors = new ValidationErrors();
 
-        validateName(createRoomDTO.getName(), validationErrors);
+        validateName(null, createRoomDTO.getName(), validationErrors);
         validateSeats(createRoomDTO.getSeats(), validationErrors);
+
+        throwOnError(validationErrors);
+    }
+
+    public void validate(Long id, UpdateRoomDTO updateRoomDTO) {
+        var validationErrors = new ValidationErrors();
+
+        validateName(id, updateRoomDTO.getName(), validationErrors);
+        validateSeats(updateRoomDTO.getSeats(), validationErrors);
 
         throwOnError(validationErrors);
     }
@@ -31,15 +44,21 @@ public class RoomValidator {
         validateMaxValue(seats, ROOM_SEATS, ROOM_SEATS_MAX_VALUE, validationErrors);
     }
 
-    private void validateName(String name, ValidationErrors validationErrors) {
+    private void validateName(Long id, String name, ValidationErrors validationErrors) {
         validateRequired(name, ROOM_NAME, validationErrors);
         validateMaxLength(name, ROOM_NAME, ROOM_NAME_MAX_LENGTH, validationErrors);
-        validateNameDuplicate(name, validationErrors);
+        validateNameDuplicate(id, name, validationErrors);
     }
 
-    private void validateNameDuplicate(String name, ValidationErrors validationErrors) {
+    private void validateNameDuplicate(Long roomIdToExclude, String name, ValidationErrors validationErrors) {
         roomRepository
             .findByNameAndActive(name, true)
-            .ifPresent(__ -> validationErrors.add(ROOM_NAME, ROOM_NAME + DUPLICATE));
+            .ifPresent(room -> {
+                if (isNull(roomIdToExclude)) {
+                    validationErrors.add(ROOM_NAME, ROOM_NAME + DUPLICATE);
+                } else if (!Objects.equals(room.getId(), roomIdToExclude)) {
+                    validationErrors.add(ROOM_NAME, ROOM_NAME + DUPLICATE);
+                }
+            });
     }
 }
