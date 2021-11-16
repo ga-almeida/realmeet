5e87924058f5881dd23550612d83ca128d676580
ga-almeida <gabrielsantos.almeida@hotmail.com>
2021-11-16 19:10:51 -0300

    validate data employee

--- service/src/main/java/br/com/sw2you/realmeet/validator/AllocationValidator.java
+++ service/src/main/java/br/com/sw2you/realmeet/validator/AllocationValidator.java
@@ -19,6 +19,8 @@ public class AllocationValidator {
         var validationErrors = new ValidationErrors();
 
         validateSubject(createAllocationDTO.getSubject(), validationErrors);
+        validateEmployeeName(createAllocationDTO.getEmployeeName(), validationErrors);
+        validateEmployeeEmail(createAllocationDTO.getEmployeeEmail(), validationErrors);
 
         throwOnError(validationErrors);
     }
@@ -26,5 +28,15 @@ public class AllocationValidator {
     private void validateSubject(String subject, ValidationErrors validationErrors) {
         validateRequired(subject, ALLOCATION_SUBJECT, validationErrors);
         validateMaxLength(subject, ALLOCATION_SUBJECT, ALLOCATION_SUBJECT_MAX_LENGTH, validationErrors);
+    }
+
+    private void validateEmployeeName(String employeeName, ValidationErrors validationErrors) {
+        validateRequired(employeeName, ALLOCATION_EMPLOYEE_NAME, validationErrors);
+        validateMaxLength(employeeName, ALLOCATION_EMPLOYEE_NAME, ALLOCATION_EMPLOYEE_NAME_MAX_LENGTH, validationErrors);
+    }
+
+    private void validateEmployeeEmail(String employeeEmail, ValidationErrors validationErrors) {
+        validateRequired(employeeEmail, ALLOCATION_EMPLOYEE_EMAIL, validationErrors);
+        validateMaxLength(employeeEmail, ALLOCATION_EMPLOYEE_EMAIL, ALLOCATION_EMPLOYEE_EMAIL_MAX_LENGTH, validationErrors);
     }
 }
--- service/src/main/java/br/com/sw2you/realmeet/validator/ValidatorConstants.java
+++ service/src/main/java/br/com/sw2you/realmeet/validator/ValidatorConstants.java
@@ -10,7 +10,9 @@ public final class ValidatorConstants {
     public static final String ALLOCATION_SUBJECT = "subject";
     public static final int ALLOCATION_SUBJECT_MAX_LENGTH = 60;
     public static final String ALLOCATION_EMPLOYEE_NAME = "employeeName";
+    public static final int ALLOCATION_EMPLOYEE_NAME_MAX_LENGTH = 20;
     public static final String ALLOCATION_EMPLOYEE_EMAIL = "employeeEmail";
+    public static final int ALLOCATION_EMPLOYEE_EMAIL_MAX_LENGTH = 30;
     public static final String ALLOCATION_ROOM_ID = "roomId";
     public static final String ALLOCATION_START_AT = "startAt";
     public static final String ALLOCATION_END_AT = "endAt";
