ca68c30aff057213c368f991fb736685f1c22a3c
ga-almeida <gabrielsantos.almeida@hotmail.com>
2021-11-21 12:53:24 -0300

    test allocation validator

--- service/src/test/java/br/com/sw2you/realmeet/unit/AllocationValidatorUnitTest.java
+++ service/src/test/java/br/com/sw2you/realmeet/unit/AllocationValidatorUnitTest.java
@@ -1,5 +1,6 @@
 package br.com.sw2you.realmeet.unit;
 
+import static br.com.sw2you.realmeet.util.DateUtils.now;
 import static br.com.sw2you.realmeet.utils.TestDataCreator.newCreateAllocationDTO;
 import static br.com.sw2you.realmeet.validator.ValidatorConstants.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -89,5 +90,62 @@ class AllocationValidatorUnitTest extends BaseUnitTest {
         );
         assertEquals(1, exception.getValidationErrors().getNumberOfErrors());
         assertEquals(new ValidationError(ALLOCATION_EMPLOYEE_EMAIL, ALLOCATION_EMPLOYEE_EMAIL + EXCEEDS_MAX_LENGTH), exception.getValidationErrors().getError(0));
+    }
+
+    @Test
+    void testValidateWhenAllocationStartAtIsMissing() {
+        var exception = assertThrows(
+                InvalidRequestException.class,
+                () -> victim.validate(newCreateAllocationDTO().startAt(null))
+        );
+        assertEquals(1, exception.getValidationErrors().getNumberOfErrors());
+        assertEquals(new ValidationError(ALLOCATION_START_AT, ALLOCATION_START_AT + MISSING), exception.getValidationErrors().getError(0));
+    }
+
+    @Test
+    void testValidateWhenAllocationStartAtInTheFuture() {
+        var exception = assertThrows(
+                InvalidRequestException.class,
+                () -> victim.validate(newCreateAllocationDTO().startAt(now().minusHours(2)).endAt(now().plusHours(1)))
+        );
+        assertEquals(1, exception.getValidationErrors().getNumberOfErrors());
+        assertEquals(new ValidationError(ALLOCATION_START_AT, ALLOCATION_START_AT + IN_THE_PAST), exception.getValidationErrors().getError(0));
+    }
+
+    @Test
+    void testValidateWhenAllocationEndtAtIsMissing() {
+        var exception = assertThrows(
+                InvalidRequestException.class,
+                () -> victim.validate(newCreateAllocationDTO().endAt(null))
+        );
+        assertEquals(1, exception.getValidationErrors().getNumberOfErrors());
+        assertEquals(new ValidationError(ALLOCATION_END_AT, ALLOCATION_END_AT + MISSING), exception.getValidationErrors().getError(0));
+    }
+
+    @Test
+    void testValidateWhenAllocationEndAtInTheFuture() {
+        var exception = assertThrows(
+                InvalidRequestException.class,
+                () -> victim.validate(newCreateAllocationDTO().startAt(now().plusHours(1)).endAt(now().minusHours(1)))
+        );
+        assertEquals(new ValidationError(ALLOCATION_END_AT, ALLOCATION_END_AT + IN_THE_PAST), exception.getValidationErrors().getError(0));
+    }
+
+    @Test
+    void testValidateWhenAllocationDateOrdering() {
+        var exception = assertThrows(
+                InvalidRequestException.class,
+                () -> victim.validate(newCreateAllocationDTO().startAt(now().plusDays(1)).endAt(now()))
+        );
+        assertEquals(new ValidationError(ALLOCATION_DATES, ALLOCATION_DATES + INCONSISTENT), exception.getValidationErrors().getError(1));
+    }
+
+    @Test
+    void testValidateWhenAllocationDuration() {
+        var exception = assertThrows(
+                InvalidRequestException.class,
+                () -> victim.validate(newCreateAllocationDTO().endAt(now().plusDays(2)))
+        );
+        assertEquals(new ValidationError(ALLOCATION_DATES, ALLOCATION_DATES + EXCEEDS_DURATION), exception.getValidationErrors().getError(0));
     }
 }
